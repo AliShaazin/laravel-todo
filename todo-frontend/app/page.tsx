aba2c294814b09aff34cb55f1987ed12a5c8145e
Ali Shaazin <ali.shazin.fareed@gmail.com>
2025-11-30 22:59:34 +0500

    await cookie

--- todo-frontend/app/page.tsx
+++ todo-frontend/app/page.tsx
@@ -8,8 +8,8 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("access_token");
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access_token");
   if (!token) {
     redirect("/login");
   }
