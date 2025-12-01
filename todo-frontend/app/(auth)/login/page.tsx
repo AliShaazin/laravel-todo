7aeba68e090c8ca54d0ddae28ec3a03802eb0292
Ali Shaazin <ali.shazin.fareed@gmail.com>
2025-12-01 16:25:55 +0500

    Revert "redirect to / if token"
    This reverts commit 7fc34b53ae961ef697a4cd2928d8824a7ecf5edf.

--- todo-frontend/app/(auth)/login/page.tsx
+++ todo-frontend/app/(auth)/login/page.tsx
@@ -1,13 +1,6 @@
 import Login from "@/components/Login";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
-const LoginPage = async () => {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("access_token");
-  if (token) {
-    redirect("/");
-  }
+const LoginPage = () => {
   return (
     <section className="flex min-h-screen w-full items-center justify-center">
       <section className="h-[50vh] w-[50vh] border rounded-xl border-gray-600 p-10 flex flex-col">
